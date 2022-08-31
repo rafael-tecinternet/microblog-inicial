af4809a077b3f4cf659413bcb84f1cde4b842b37
DR4CH3N <thehellc4t@gmail.com>
2022-08-31 10:56:22 -0300

    adicionados metodos e variaveis de token e email
    adicionados metodos para encriptacao do token pelo password_hash e adicionado mensagem de reset de senha para mandar para o email do usuario

--- inc/reset-senha.php
+++ inc/reset-senha.php
@@ -19,7 +19,52 @@ if (isset($_POST["reset-senha"])) {
 
     $expiracao = date("U") + 600;
 
+    /* aqui ele vai fazer a conexao com o banco, por isso o require */
+    require 'Banco.php';
+
+    /* ele vai pegar o campo email do formulario */
+    $emailUsuario = $_POST["email"];
+
+    /* aqui ele ira deletar o token (caso tiver mais de um) do banco de dados do usuario, 
+    a interrogacao depois do pwdResetEmail sera substituida pela variavel $declaracao, 
+    vai vir no tipo string e vai receber tambem o email do usuario*/
+    $sql = "DELETE FROM resetsenha WHERE pwdResetEmail=?";
+    $declaracao = mysqli_stmt_init($conexao);
+
+    if (!mysqli_stmt_prepare($declaracao, $sql) ) {
+        echo "OPS! houve um erro, tente novamente";
+        die();
+    } else {
+        /* o "s" significa string */
+        mysqli_stmt_bind_param($declaracao, "s", $emailUsuario);
+        mysqli_stmt_execute($declaracao);
+    }
     
+    $sql = "INSERT INTO resetsenha (pwdResetEmail, pwdResetSelecionador, pwdResetToken, pwdResetExpiracao) VALUES (?, ?, ?, ?);";
+    $declaracao = mysqli_stmt_init($conexao);
+
+    if (!mysqli_stmt_prepare($declaracao, $sql) ) {
+        echo "OPS! houve um erro, tente novamente";
+        die();
+    } else {
+        /* aqui ele vai fazer a encriptacao to token com a funcao de password hash */
+        $hashedToken = password_hash($token, PASSWORD_DEFAULT);
+
+        /* o "s" significa string */
+        mysqli_stmt_bind_param($declaracao, "ssss", $emailUsuario, $selecionador, $hashedToken, $expiracao);
+        mysqli_stmt_execute($declaracao);
+    }
+
+    mysqli_stmt_close($declaracao);
+    
+    $emailPara = $emailUsuario;
+    
+    $assunto = "Redefinir sua senha na Calor dado";
+
+    $mensagem = '<p>Recebemos seu pedido para mudar sua senha, o link para poder muda-la está logo abaixo. se você não fez esse pedido sugerimos que você troque sua senha pois ela pode estar comprometida.</p>';
+    $mensagem .= '<p>Aqui esta o link para poder mudar sua senha: </br>';
+    $mensagem .= '<a href="' . $url . '">' . $url . '</a></p>';
+
 } else {
     // caso o usuario entrar nessa pagina de recuperacao de senha por engano, ele vai para a localizacao informada
     header("location: ../index.php");
