98dc97cf587368b4daf4f61674c3adcefbdbe816
DR4CH3N <thehellc4t@gmail.com>
2022-08-31 10:01:24 -0300

    adicionada data de expiracao ao token

--- inc/reset-senha.php
+++ inc/reset-senha.php
@@ -9,6 +9,17 @@ if (isset($_POST["reset-senha"])) {
     $selecionador = bin2hex(random_bytes(8));
     $token = random_bytes(32);
 
+    /* a variavel URL vai pegar a pagina de recuperar a senha e concatenar com as variaveis $selecionador e $token */
+
+    $url = "localhost/microblog-inicial/esqueceu-sua-senha.php?selecionador=".$selecionador. "&validator=". bin2hex($token);
+
+    /* criando uma data de expiracao para o token */
+
+    /* aqui, a data de expiracao vai ser 10 minutos (600 segundos) apos o token ser criado, o usuario tera 10min para poder fazer o reset de sua senha */
+
+    $expiracao = date("U") + 600;
+
+    
 } else {
     // caso o usuario entrar nessa pagina de recuperacao de senha por engano, ele vai para a localizacao informada
     header("location: ../index.php");
